a2f52bca3a8bf5292e08d7e4c4480a4eb664c73b
NANY <NANY@DESKTOP-O16UFLN>
2020-04-02 17:26:09 -0600

    #1 Inicio de proyecto

---
+++ src/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NANY
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("NOMBRE:");
+        System.out.println("Cesar Arath Rivera Diaz");
+        System.out.println("ZONA:");
+        System.out.println("Colonia las Victorias cambote zona 11");
+        System.out.println("NUMERO DE TELEFONO:");
+        System.out.println("59269929");
+    }
+    
+}
