468b71e396b34d4ede52406a1f5cc3c1fee4ec19
LuisRuiz16 <62570482+LuisRuiz16@users.noreply.github.com>
2020-04-02 18:01:06 -0600

    Update main.java

--- src/main.java
+++ src/main.java
@@ -41,6 +41,10 @@ public class main {
         System.out.println("Daniel Alonzo Herrera ");
         System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
         System.out.println("3059-6671 ");
+     
+        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
+        System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
+        System.out.println("Telefono: 37070944");
         
     }
     
